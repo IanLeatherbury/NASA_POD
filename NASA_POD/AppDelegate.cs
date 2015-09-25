ec909514187211b49242806670a982c00475d7ff
Ian Leatherbury <ianl@ianl-mac.local>
2015-09-25 14:19:20 -0700

    updates

--- NASA_POD/AppDelegate.cs
+++ NASA_POD/AppDelegate.cs
@@ -7,6 +7,8 @@ using System.IO;
 using System;
 using CoreGraphics;
 using System.Drawing;
+using Xamarin;
+using System.Collections.Generic;
 
 namespace NASA_POD
 {
@@ -18,10 +20,10 @@ namespace NASA_POD
 			set;
 		}
 
-		public override void ReceivedLocalNotification(UIApplication application, UILocalNotification notification)
+		public override void ReceivedLocalNotification (UIApplication application, UILocalNotification notification)
 		{
 			// show an alert
-			new UIAlertView(notification.AlertAction, "!" + notification.AlertBody, null, "OK", null).Show();
+			new UIAlertView (notification.AlertAction, "!" + notification.AlertBody, null, "OK", null).Show ();
 
 			// reset our badge
 			UIApplication.SharedApplication.ApplicationIconBadgeNumber = 0;
@@ -30,7 +32,7 @@ namespace NASA_POD
 		public override void HandleAction (UIApplication application, string actionIdentifier, UILocalNotification localNotification, Action completionHandler)
 		{
 			// show an alert
-			new UIAlertView(localNotification.AlertAction, "?" + localNotification.AlertBody, null, "OK", null).Show();
+			new UIAlertView (localNotification.AlertAction, "?" + localNotification.AlertBody, null, "OK", null).Show ();
 
 			// reset our badge
 			UIApplication.SharedApplication.ApplicationIconBadgeNumber = 0;
@@ -46,6 +48,63 @@ namespace NASA_POD
 			Xamarin.Calabash.Start ();
 			#endif
 
+//			Insights.Initialize("1927149b6a7581f424fb6fc8ded1cfc54c9e6c10"); //old insights
+			Insights.Initialize ("13edaee3613130296848d5406ed1b8028b1136b7"); // insight DF session
+
+			#region InsightDFSession
+			//Identify
+			Insights.Identify ("a.unique.id@for.this.user", Insights.Traits.Name, "Gordon Strachen");
+			var traits = new Dictionary<string, string> {
+				{ Insights.Traits.Email, "gordon.strachen@celtic.com" },
+				{ Insights.Traits.Name, "Gordon Strachen" }
+			};
+			Insights.Identify ("YourUsersUniqueId", traits);
+
+			//track
+			Insights.Track ("MusicTrackPlayed", new Dictionary<string, string> {
+				{ "SongName", "Shake It Off" },
+				{ "Length", "219" }
+			});
+
+			//error
+			try {
+				throw new NotSupportedException (string.Format ("Oh no. Really bad stuff happened.", 1));
+			} catch (Exception exp) {
+				Xamarin.Insights.Report (exp, new Dictionary <string, string> { 
+					{ "error-local-time", DateTime.Now.ToString () }
+				}, Xamarin.Insights.Severity.Error);
+			}
+
+			//warning
+			try {
+				// create a collection container to hold exceptions
+				List<Exception> exceptions = new List<Exception> ();
+
+				// do some stuff here ........
+
+				// we have an exception with an innerexception, so add it to the list
+				exceptions.Add (new TimeoutException ("This is part 1 of aggregate exception", new ArgumentException ("ID missing")));
+
+				// do more stuff .....
+
+				// Another exception, add to list
+				exceptions.Add (new NotImplementedException ("This is part 2 of aggregate exception"));
+
+				// all done, now create the AggregateException and throw it
+				AggregateException aggEx = new AggregateException (exceptions);
+				//throw aggEx;
+				throw aggEx;
+			} catch (Exception exp) {
+				Xamarin.Insights.Report (exp, new Dictionary <string, string> { 
+					{ "warning-local-time", DateTime.Now.ToString () }
+				}, Xamarin.Insights.Severity.Warning);
+			}
+
+//			throw new Exception("Unhandled Exception that crashed your app.");
+
+			#endregion InsightsDFSession
+
+
 			//ask for notification priveleges
 			var settings = UIUserNotificationSettings.GetSettingsForTypes (
 				               UIUserNotificationType.Alert | UIUserNotificationType.Badge |
@@ -53,15 +112,12 @@ namespace NASA_POD
 			UIApplication.SharedApplication.RegisterUserNotificationSettings (settings);
 
 			// check for a notification
-			if (launchOptions != null)
-			{
+			if (launchOptions != null) {
 				// check for a local notification
-				if (launchOptions.ContainsKey(UIApplication.LaunchOptionsLocalNotificationKey))
-				{
-					var localNotification = launchOptions[UIApplication.LaunchOptionsLocalNotificationKey] as UILocalNotification;
-					if (localNotification != null)
-					{
-						new UIAlertView(localNotification.AlertAction, localNotification.AlertBody, null, "OK", null).Show();
+				if (launchOptions.ContainsKey (UIApplication.LaunchOptionsLocalNotificationKey)) {
+					var localNotification = launchOptions [UIApplication.LaunchOptionsLocalNotificationKey] as UILocalNotification;
+					if (localNotification != null) {
+						new UIAlertView (localNotification.AlertAction, localNotification.AlertBody, null, "OK", null).Show ();
 						// reset our badge
 						UIApplication.SharedApplication.ApplicationIconBadgeNumber = 0;
 					}
